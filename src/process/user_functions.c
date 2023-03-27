1481cf70f6e692fb1dbc5ae0356a174196b4edb9
rishighia <ghiar@seas.upenn.edu>
2023-03-27 18:49:46 -0400

    added sleep func

--- src/process/user_functions.c
+++ src/process/user_functions.c
@@ -1,4 +1,6 @@
 #include "user_functions.h"
+#include <time.h>
+
 
 void echoFunc(int argc, char *argv[]) {
     int i;
@@ -15,4 +17,12 @@ void echoFunc(int argc, char *argv[]) {
     // Print the message string to stdout
     printf("%s\n", message);
     
+}
+
+
+void sleepFunc(int milliseconds) {
+    clock_t start_time = clock();
+    while ((clock() - start_time) * 1000 / CLOCKS_PER_SEC < milliseconds) {
+        // do nothing
+        }
 }
